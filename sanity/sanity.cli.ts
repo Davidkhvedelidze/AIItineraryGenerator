4efab80615f03f236bcb7d32a2fe760cc51e5cbb
Davit Khvedelidze <dkhvedelidze@dga.gov.ge>
2026-05-05 15:31:25 +0400

    fix: remove deployment configuration and ensure dataset property ends with a comma

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,13 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'fsb01gwc',
-    dataset: 'production'
+    dataset: 'production',
   },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
-  }
 })
