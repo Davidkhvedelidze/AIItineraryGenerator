fdd41f693c08081c9d655136c0ea1509c96eb8e4
Davit Khvedelidze <dkhvedelidze@dga.gov.ge>
2026-05-05 15:34:36 +0400

    fix: add deployment configuration for auto-updates and update tsconfig exclusions

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -5,4 +5,11 @@ export default defineCliConfig({
     projectId: 'fsb01gwc',
     dataset: 'production',
   },
+  deployment: {
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  },
 })
